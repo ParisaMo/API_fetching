dcc4d66f1cbb603819b4e42fd70bc1438ae1af17
Parisa Mohammadzadeh <125735507+ParisaMo@users.noreply.github.com>
2024-06-21 13:38:13 -0700

    Update app.py

--- practice1-applyinganAPI/app.py
+++ practice1-applyinganAPI/app.py
@@ -4,7 +4,7 @@ import requests
 # Initialize the Flask application
 app = Flask(__name__)
 
-API_KEY = "DEMO_KEY" 
+API_KEY = "DEMO_KEY"
 
 # Define a route to fetch and display the list of food items
 @app.route('/')
@@ -28,11 +28,12 @@ def food_detail(fdcId):
         response = requests.get(api_url)
         response.raise_for_status()
         food_details = response.json()
+        print(food_details)  
     except requests.RequestException as e:
         return f"Error fetching data from the API: {str(e)}", 500
 
-    # Render the detail HTML template and pass the food details to it
     return render_template('detail.html', food=food_details)
+
 
 # Run the Flask application
 if __name__ == '__main__':
